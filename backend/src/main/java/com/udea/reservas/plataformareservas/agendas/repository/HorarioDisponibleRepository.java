12b1c64717c9914f5448c885a73751583096c722
soydz <dev@soydz.com>
2026-09-18 18:16:12 -0500

    refactor: convertir servicios a interfaces con implementaciones

--- backend/src/main/java/com/udea/reservas/plataformareservas/agendas/repository/HorarioDisponibleRepository.java
+++ backend/src/main/java/com/udea/reservas/plataformareservas/agendas/repository/HorarioDisponibleRepository.java
@@ -1,7 +1,7 @@
 package com.udea.reservas.plataformareservas.agendas.repository;
 
-import com.udea.reservas.plataformareservas.agendas.model.HorarioDisponible;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface HorarioDisponibleRepository extends JpaRepository<HorarioDisponible, Integer> {
-}
+@Repository
+public interface HorarioDisponibleRepository
+    extends IHorarioDisponibleRepository {}
